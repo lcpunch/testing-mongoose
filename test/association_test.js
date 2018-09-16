5d0d77a26042ab20f72b11e831cf174a175e2a6e
lcpunch <lcpunchirolli@gmail.com>
2018-09-16 01:34:51 -0400

    Populating queries

--- test/association_test.js
+++ test/association_test.js
@@ -5,14 +5,30 @@ const BlogPost = require('../src/blogPost');
 
 describe('Association', () => {
   let joe, blogPost, comment;
-  beforeEach(() => {
+  beforeEach((done) => {
     joe = new User({ name: 'Joe' });
     blogPost = new BlogPost({ title: 'JS is great', content: 'yes it is' });
     comment = new Comment({ content: 'Thats really true my friend' });
 
     // Magic. Mongo will automatically associate the ObjectId instead of the object.
     joe.blogPosts.push(blogPost);
-    blogPosts.comments.push(comment);
+    blogPost.comments.push(comment);
     comment.user = joe;
+
+    Promise.all([
+      joe.save(),
+      blogPost.save(),
+      comment.save()
+    ])
+    .then(() => done());
+  });
+
+  it.only('saves a relation between a user and a blogpost', (done) => {
+    User.findOne({ name: 'Joe' })
+      .populate('blogPosts')
+      .then((user) => {
+        assert(user.blogPosts[0].title === blogPost.title)
+        done();
+      });
   });
 });
