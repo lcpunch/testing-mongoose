1921aae0c55d5ca65a10d058b05a60a18273e868
lcpunch <lcpunchirolli@gmail.com>
2018-09-16 00:32:09 -0400

    Fixed test

--- test/subdocument_test.js
+++ test/subdocument_test.js
@@ -38,4 +38,24 @@ describe('Subdocuments', () => {
         done();
       });
   });
+
+  it('Can remove an existing subdocument', (done) => {
+    const joe = new User({
+      name: 'Joe',
+      posts: [{ title: 'New title' }]
+    });
+
+    joe.save()
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        const post = user.posts[0];
+        post.remove();
+        return user.save();
+      })
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(user.posts.length === 0);
+        done();
+      });
+  });
 });
